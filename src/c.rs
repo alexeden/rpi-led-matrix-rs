c63a448899659ced2377a8f533884bb2547190fa
Alex Eden <alexandereden91@gmail.com>
2020-09-25 01:00:37 -0500

    Start writing the wrapper functions

--- src/c.rs
+++ src/c.rs
@@ -1,10 +1,34 @@
 use super::options::{LedMatrixOptions, LedRuntimeOptions};
 use libc::c_int;
 
-#[allow(dead_code)]
-pub enum LedCanvas {}
+#[derive(Debug)]
+pub struct LedColor {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
 #[allow(dead_code)]
 pub enum LedMatrix {}
+
+pub enum LedCanvas {}
+
+#[allow(dead_code)]
+impl LedCanvas {
+    pub fn get_size(&self) -> (i32, i32) {
+        let (w, h): (c_int, c_int) = (0, 0);
+        unsafe {
+            led_canvas_get_size(self, w as *mut c_int, h as *mut c_int);
+        }
+        (w as i32, h as i32)
+    }
+
+    pub fn set(&mut self, x: i32, y: i32, color: &LedColor) {
+        unsafe {
+            led_canvas_set_pixel(self, x, y, color.r, color.g, color.b);
+        }
+    }
+}
 
 #[allow(dead_code)]
 #[link(name = "rgbmatrix")]
@@ -43,4 +67,28 @@ extern "C" {
         g: u8,
         b: u8,
     );
+// pub fn load_font(bdf_font_file: *const c_char) -> *mut LedFont;
+// pub fn delete_font(font: *mut LedFont);
+// pub fn draw_text(
+//     canvas: *mut LedCanvas,
+//     font: *const LedFont,
+//     x: c_int,
+//     y: c_int,
+//     r: u8,
+//     g: u8,
+//     b: u8,
+//     utf8_text: *const c_char,
+//     kerning_offset: c_int,
+// ) -> c_int;
+// pub fn vertical_draw_text(
+//     canvas: *mut LedCanvas,
+//     font: *const LedFont,
+//     x: c_int,
+//     y: c_int,
+//     r: u8,
+//     g: u8,
+//     b: u8,
+//     utf8_text: *const c_char,
+//     kerning_offset: c_int,
+// ) -> c_int;
 }
