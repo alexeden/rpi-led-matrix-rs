41ff2130fa945c03f504133334ab464c5d3ec058
Alex Eden <alexandereden91@gmail.com>
2020-09-25 18:05:28 -0500

    Make up a config file to be used for syncing

--- sync.rs
+++ sync.rs
@@ -1,8 +1,16 @@
-//! ```cargo
-//! [dependencies]
-//! rusync = "*"
-//! ```
-use rusync;
+//! rsync
+//!     -ahPr
+//!     --rsh={command}
+//!     --exclude=.git
+//!     --exclude=.DS_Store
+//!     --exclude=node_modules
+//!     --exclude=build
+//!     --exclude=package-lock.json
+//!     --exclude=.vscode
+//!     --exclude=dist
+//!     /Users/alexeden/code/rpi-led-matrix
+//!     pi@192.168.1.201:~
+
 
 fn main() {
     let console_info = rusync::ConsoleProgressInfo::new();
