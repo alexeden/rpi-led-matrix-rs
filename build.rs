5c2af97e0a6270a628a2b68fe4006c2723c1ee84
Alex Eden <alexandereden91@gmail.com>
2020-09-26 01:04:01 -0500

    Cleanup

--- build.rs
+++ build.rs
@@ -1,48 +1,16 @@
 #[cfg(target_os = "linux")]
 use std::process::Command;
 
+#[cfg(not(target_os = "linux"))]
+fn main() {}
+
+#[cfg(target_os = "linux")]
 fn main() {
     println!("cargo:rerun-if-changed=./extern");
     println!("cargo:rustc-flags=-l dylib=stdc++");
     println!("cargo:rustc-link-search=./extern/lib");
-    #[cfg(target_os = "linux")]
-    Command::new("make").current_dir("./extern").status().unwrap();
+    Command::new("make")
+        .current_dir("./extern")
+        .status()
+        .unwrap();
 }
-
-
-// extern crate cc;
-
-// #[cfg(not(target_os = "linux"))]
-// fn build_extern() {}
-
-// #[cfg(target_os = "linux")]
-// fn build_extern() {
-//     let compilation = cc::Build::new()
-//         .cpp(true)
-//         .flag("-Wno-missing-field-initializers")
-//         .flag("-Wno-unused-parameter")
-//         .flag("-ltr")
-//         .flag("-lm")
-//         .flag("-lpthread")
-//         .include("./extern/include")
-//         .files(&[
-//             "./extern/lib/thread.cc",
-//             "./extern/lib/pixel-mapper.cc",
-//             "./extern/lib/options-initialize.cc",
-//             "./extern/lib/multiplex-mappers.cc",
-//             "./extern/lib/led-matrix-c.cc",
-//             "./extern/lib/led-matrix.cc",
-//             "./extern/lib/graphics.cc",
-//             "./extern/lib/gpio.cc",
-//             "./extern/lib/framebuffer.cc",
-//             "./extern/lib/content-streamer.cc",
-//             "./extern/lib/bdf-font.cc",
-//             "./extern/lib/hardware-mapping.c",
-//         ])
-//         .target("librgbmatrix")
-//         .try_compile("librgbmatrix");
-
-//     if let Err(e) = compilation {
-//         println!("cargo:warning={:?}", e);
-//     }
-// }
--- examples/minimal.rs
+++ examples/minimal.rs
@@ -1,34 +1,5 @@
-use rpi_led_matrix::{matrix::LedMatrix, options::LedMatrixOptions};
+use rpi_led_matrix::matrix::LedMatrix;
 
 fn main() {
-    println!("STARTING MINIMAL EXAMPLE");
-    let mut mat_opts = LedMatrixOptions::default();
-    mat_opts.set_panel_type("");
-    mat_opts.set_led_rgb_sequence("RGB");
-    println!("{:?}", mat_opts);
-    let _mat = LedMatrix::new(Some(mat_opts), None).expect("Matrix creation");
-    // let _canvas = mat.canvas();
+    LedMatrix::new(None, None).expect("Matrix creation");
 }
-
-
-// LedMatrixOptions {
-//     brightness: 100,
-//     chain_length: 1,
-//     cols: 32,
-//     disable_hardware_pulsing: 0,
-//     hardware_mapping: 0x556e7bc5b0,
-//     inverse_colors: 0,
-//     led_rgb_sequence: 0x556e7b7790,
-//     limit_refresh_rate_hz: 0,
-//     multiplexing: 0,
-//     panel_type: 0x556e7bcfb0,
-//     parallel: 1,
-//     pixel_mapper_config: 0x556e7bce00,
-//     pwm_bits: 11,
-//     pwm_dither_bits: 0,
-//     pwm_lsb_nanoseconds: 130,
-//     row_address_type: 0,
-//     rows: 32,
-//     scan_mode: 0,
-//     show_refresh_rate: 0,
-// }
