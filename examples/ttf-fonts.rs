06450b06f5c24dca182b1009f5cc8fa8950d574c
Alex Eden <alexandereden91@gmail.com>
2020-09-27 03:09:11 -0500

    Add rusttype as dep, create example for testing use of TTF fonts

--- examples/ttf-fonts.rs
+++ examples/ttf-fonts.rs
@@ -1,8 +1,3 @@
-use embedded_graphics::{
-    drawable::Drawable, fonts::Font6x8, fonts::Text, pixelcolor::Rgb565, pixelcolor::RgbColor,
-    prelude::Point, prelude::Primitive, primitives::Circle, style::PrimitiveStyle,
-    style::TextStyle,
-};
 use rpi_led_matrix::{
     matrix::LedMatrix, matrix_options::GpioMapping, matrix_options::LedMatrixOptions,
     runtime_options::LedRuntimeOptions,
@@ -25,14 +20,4 @@ fn main() {
     let rt_options = LedRuntimeOptions::default().set_gpio_slowdown(4);
 
     let mut mat = LedMatrix::new(Some(mat_options), Some(rt_options)).expect("Matrix creation");
-
-    let c = Circle::new(Point::new(20, 20), 8).into_styled(PrimitiveStyle::with_fill(Rgb565::RED));
-    let t = Text::new("Hello Rust!", Point::new(20, 16))
-        .into_styled(TextStyle::new(Font6x8, Rgb565::GREEN));
-    // c.translate(by: Point)
-    c.draw(&mut mat).unwrap();
-    t.draw(&mut mat).unwrap();
-
-    mat.sync();
-    wait(30000);
 }
