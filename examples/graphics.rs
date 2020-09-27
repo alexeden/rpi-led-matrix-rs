2731e3110896ae69e6b3297e98262914da1b4424
Alex Eden <alexandereden91@gmail.com>
2020-09-27 02:10:45 -0500

    Start building a new example for testing emb graphics

---
+++ examples/graphics.rs
@@ -0,0 +1,23 @@
+use rpi_led_matrix::{
+    matrix::LedMatrix, matrix_options::GpioMapping, matrix_options::LedMatrixOptions,
+    runtime_options::LedRuntimeOptions,
+};
+
+fn wait(msecs: u64) {
+    std::thread::sleep(std::time::Duration::from_millis(msecs));
+}
+
+fn main() {
+    let mat_options = LedMatrixOptions::default()
+        .set_cols(64)
+        .set_rows(32)
+        .set_chain_length(2)
+        .set_hardware_mapping(GpioMapping::Regular)
+        .set_pwm_lsb_nanoseconds(130)
+        .set_pwm_dither_bits(0)
+        .set_parallel(1);
+
+    let rt_options = LedRuntimeOptions::default().set_gpio_slowdown(4);
+
+    let mut mat = LedMatrix::new(Some(mat_options), Some(rt_options)).expect("Matrix creation");
+}
