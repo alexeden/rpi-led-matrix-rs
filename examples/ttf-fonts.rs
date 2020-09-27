c1cae1fbf9c60883ec975e10fbc125e3d17c5dfd
Alex Eden <alexandereden91@gmail.com>
2020-09-27 17:15:34 -0500

    Use roboto font, add scrolling effect to TTF example

--- examples/ttf-fonts.rs
+++ examples/ttf-fonts.rs
@@ -5,9 +5,10 @@ use example_utils::{create_matrix, wait};
 
 fn main() {
     let mut mat = create_matrix();
+    mat.brightness_set(100);
 
     let font = {
-        let font_path = std::env::current_dir().unwrap().join("examples/montserrat.ttf");
+        let font_path = std::env::current_dir().unwrap().join("examples/roboto.ttf");
         let data = std::fs::read(&font_path).unwrap();
         Font::try_from_vec(data).unwrap_or_else(|| {
             panic!(format!(
@@ -23,24 +24,29 @@ fn main() {
     // VMetrics { ascent: 12.705497, descent: -3.2945037, line_gap: 0.0, }
     let v_metrics = font.v_metrics(scale);
 
-    font.layout("Serving TTF lewks.", scale, point(2., v_metrics.ascent))
-        .for_each(|glyph| {
-            if let Some(bounding_box) = glyph.pixel_bounding_box() {
-                // Draw the glyph into the image per-pixel by using the draw closure
-                glyph.draw(|x, y, v| {
-                    // Threshold the brightness of each text pixel.
-                    // TODO: I bet running a convolution pass over the glyphs' alphas would make this
-                    // a lot better than having a hand-tuned value.
-                    let alpha = if v < 0.3 { 0 } else { 255 };
-                    mat.set(
-                        (x + bounding_box.min.x as u32) as i32,
-                        (y + bounding_box.min.y as u32) as i32,
-                        &Color::of(alpha, alpha, alpha),
-                    );
-                });
-            }
-        });
+    (0..).for_each(|dx| {
+        mat.clear();
+        font.layout("Serving TTF lewks.", scale, point(0., v_metrics.ascent))
+            .for_each(|glyph| {
+                if let Some(bounding_box) = glyph.pixel_bounding_box() {
+                    // Draw the glyph into the image per-pixel by using the draw closure
+                    glyph.draw(|x, y, v| {
+                        let alpha = (255. * v) as u8;
+                        let mut x = x as i32 - dx + bounding_box.min.x;
 
-    mat.sync();
-    wait(120000);
+                        while x < 0 {
+                            x = x + mat.width()
+                        }
+
+                        mat.set(
+                            x,
+                            y as i32 + bounding_box.min.y,
+                            &Color::of(alpha, alpha, alpha),
+                        );
+                    });
+                }
+            });
+        mat.sync();
+        wait(10);
+    });
 }
