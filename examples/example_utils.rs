3aec8d91c380a6ef82022a11f5a88140b4c46d1f
Alex Eden <alexandereden91@gmail.com>
2020-09-27 15:05:14 -0500

    Create a set of utils to reduce example boilerplate

---
+++ examples/example_utils.rs
@@ -0,0 +1,29 @@
+/// Not an actual example.
+/// There's enough boilerplate involved in creating a matrix with a lot of custom
+/// options that I'm creating this function that can be used by other examples.
+use rpi_led_matrix::{
+    matrix::LedMatrix, matrix_options::GpioMapping, matrix_options::LedMatrixOptions,
+    runtime_options::LedRuntimeOptions,
+};
+
+pub fn create_matrix() -> LedMatrix {
+    let mat_options = LedMatrixOptions::default()
+        .set_cols(64)
+        .set_rows(32)
+        .set_chain_length(2)
+        .set_hardware_mapping(GpioMapping::Regular)
+        .set_pwm_lsb_nanoseconds(130)
+        .set_pwm_dither_bits(0)
+        .set_parallel(1);
+
+    let rt_options = LedRuntimeOptions::default().set_gpio_slowdown(4);
+
+    LedMatrix::new(Some(mat_options), Some(rt_options)).expect("Matrix creation")
+}
+
+pub fn wait(msecs: u64) {
+    std::thread::sleep(std::time::Duration::from_millis(msecs));
+}
+
+#[allow(dead_code)]
+fn main() {}
--- examples/graphics.rs
+++ examples/graphics.rs
@@ -3,28 +3,11 @@ use embedded_graphics::{
     prelude::Point, prelude::Primitive, primitives::Circle, style::PrimitiveStyle,
     style::TextStyle,
 };
-use rpi_led_matrix::{
-    matrix::LedMatrix, matrix_options::GpioMapping, matrix_options::LedMatrixOptions,
-    runtime_options::LedRuntimeOptions,
-};
-
-fn wait(msecs: u64) {
-    std::thread::sleep(std::time::Duration::from_millis(msecs));
-}
+mod example_utils;
+use example_utils::{create_matrix, wait};
 
 fn main() {
-    let mat_options = LedMatrixOptions::default()
-        .set_cols(64)
-        .set_rows(32)
-        .set_chain_length(2)
-        .set_hardware_mapping(GpioMapping::Regular)
-        .set_pwm_lsb_nanoseconds(130)
-        .set_pwm_dither_bits(0)
-        .set_parallel(1);
-
-    let rt_options = LedRuntimeOptions::default().set_gpio_slowdown(4);
-
-    let mut mat = LedMatrix::new(Some(mat_options), Some(rt_options)).expect("Matrix creation");
+    let mut mat = create_matrix();
 
     let c = Circle::new(Point::new(20, 20), 8).into_styled(PrimitiveStyle::with_fill(Rgb565::RED));
     let t = Text::new("Hello Rust!", Point::new(20, 16))
