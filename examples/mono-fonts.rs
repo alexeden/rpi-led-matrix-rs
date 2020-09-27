5bc6173816cb1ff0d8c1c1a6b0562c5229c0601e
Alex Eden <alexandereden91@gmail.com>
2020-09-27 18:28:20 -0500

    I think I got what I needed from the mono font example

--- examples/mono-fonts.rs
+++ examples/mono-fonts.rs
@@ -1,7 +1,29 @@
 mod example_utils;
+use embedded_graphics::fonts::Font6x8;
+use embedded_graphics::{drawable::Drawable, egtext, prelude::Font, text_style};
 use example_utils::{create_matrix, wait};
-fn main() {
-    let mat = create_matrix();
+use rpi_led_matrix::led::Color;
 
-    wait(1000);
+fn main() {
+    let mut mat = create_matrix();
+    let colors = [
+        Color::of(255, 0, 0),
+        Color::of(0, 255, 0),
+        Color::of(0, 0, 255),
+        Color::of(255, 255, 255),
+    ];
+
+    (0..colors.len()).for_each(|i| {
+        let styled_text = egtext!(
+            text = "TSLA +412.05",
+            top_left = (0, i as i32 * Font6x8::CHARACTER_SIZE.height as i32),
+            style = text_style!(font = Font6x8, text_color = colors[i]),
+        );
+
+        styled_text.draw(&mut mat).unwrap();
+    });
+
+    mat.sync();
+
+    wait(120000);
 }
